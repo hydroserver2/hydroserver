342c2ee996e563407af7098c82ed65f533457fe3
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2026-07-14 22:10:12 -0700

    Updated version number

--- django/hydroserver/__init__.py
+++ django/hydroserver/__init__.py
@@ -1,9 +1,4 @@
 from .celery import app as celery_app
 
-
-<<<<<<< Updated upstream
-__version__ = "1.10.4"
-=======
 __version__ = "1.11.0b2"
->>>>>>> Stashed changes
 __all__ = ("celery_app", "__version__",)
