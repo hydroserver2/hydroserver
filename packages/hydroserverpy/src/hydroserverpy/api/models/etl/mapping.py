9396c297c1f8041436f393024ceb3c1dd1608ab5
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2026-08-14 11:38:43 -0700

    Fix target datastream mapping issue in hydroserverpy

--- packages/hydroserverpy/src/hydroserverpy/api/models/etl/mapping.py
+++ packages/hydroserverpy/src/hydroserverpy/api/models/etl/mapping.py
@@ -1,10 +1,14 @@
 import uuid
-from pydantic import BaseModel, ConfigDict
+from pydantic import AliasChoices, AliasPath, BaseModel, ConfigDict, Field
 from pydantic.alias_generators import to_camel
 
 
 class EtlMapping(BaseModel):
     source_identifier: str
-    target_datastream_id: uuid.UUID
+    target_datastream_id: uuid.UUID = Field(
+        validation_alias=AliasChoices(
+            "targetDatastreamId", AliasPath("targetDatastream", "id")
+        )
+    )
 
     model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
