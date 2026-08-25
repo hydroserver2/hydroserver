4448fde3b51824c3e5c33a4ed5021368830fdac2
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2026-08-25 15:36:19 -0700

    Fixed migration conflict

--- django/core/sta/migrations/0015_rename_metadata_table_fields.py
+++ django/core/sta/migrations/0015_rename_metadata_table_fields.py
@@ -75,7 +75,7 @@ def prepare_reverse_migration(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("sta", "0013_rename_sensor_method"),
+        ("sta", "0014_monitoringsite_tags_datastream_tags"),
     ]
 
     operations = [
